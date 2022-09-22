7771123fff6c21365f99823bd46dd5091ed6a490
Nesho RADEV <40929562+RedPRO16@users.noreply.github.com>
2022-09-22 14:41:43 +0100

    currency format added

--- lib/printStatement.rb
+++ lib/printStatement.rb
@@ -13,8 +13,14 @@ class PrintStatement
   def print(transactions)
     puts header
     transactions.reverse.each do |item|
-      puts "#{item.date} || #{item.credit} || #{item.debit} || #{item.balance}"
+      puts "£#{item.date} || £#{item.credit} || £#{item.debit} || £#{item.balance}"
     end
 
   end
+end
+
+private
+
+def to_pounds(amount)
+  "£#{format("%.2f", amount.to_f)}"
 end
--- lib/transferActivity.rb
+++ lib/transferActivity.rb
@@ -10,14 +10,14 @@ class TransferActivity
   end
 
   def add_transaction(date, credit, debit, balance)
-    format_balance = balance_two_decimals(balance)
+    format_balance = balance_two_decimals( balance)
     new_transaction = @transaction_class.new(date, credit, debit, format_balance)
     @all_transactions.push(new_transaction)
   end
 
   private
 
-  def balance_two_decimals(balance)
+  def balance_two_decimals( balance)
     format('%.2f', balance)
   end
 end
